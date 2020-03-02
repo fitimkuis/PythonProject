a5fc92bdc7bf755db123ce81bcc4786d827eefa9
fitim <fitimkuis@gmail.com>
2020-03-02 22:23:59 +0200

    second way to parse log

--- .idea/com/python/TestSuite.robot
+++ .idea/com/python/TestSuite.robot
@@ -6,8 +6,66 @@ Library          String
 Library          Collections
 Variables        .idea/com/python/dict_variables.py
 Library          .idea/com/scripts/parser_log.py
+Library           Timer
+Test Setup        Benchmark Setup
+Test Teardown     Benchmark TearDown
+
+#Library           GuiStatusKeywords
+#Suite Setup       Start Status UI
+#Suite Teardown    Stop Status UI
 
 *** Test Cases ***
+Start And Stop
+  Status UI Log               GuiStatus Demo
+  ${count}=                   Set Variable    50
+  Status UI Progressbar       ${count}
+  :FOR   ${tick}   IN RANGE    ${count}
+      Status UI Log           Processing ${tick}/${count}
+      Status UI Action        step
+  END
+
+Example No 1 Passes
+  Sleep   1 second
+
+Example No 2 Passes
+  Sleep   2 second
+
+Verify So Far
+  [Setup]       No Operation
+  [Teardown]    No Operation
+  Verify all Timers
+
+Example No 3 Will Fail
+  [Teardown]    No Operation
+  Sleep   3 second
+  Stop Timer    ${TEST NAME}
+  Run Keyword And Expect Error     STARTS: Difference     Verify Single Timer   3   0    ${TEST NAME}
+
+Final Verify Failure
+  [Setup]       No Operation
+  [Teardown]    No Operation
+  Run Keyword And Expect Error     STARTS: Difference      Verify All Timers
+
+Final Verify Without Failing
+  [Setup]       No Operation
+  [Teardown]    No Operation
+  Verify All Timers   False
+
+Can Start Timer
+    Start Timer
+
+Can Stop Timer
+    Stop Timer
+
+Can Check The Results
+    ${res}=    Verify Single Timer   5   0
+    Should Be True    ${res}
+
+Can Throw Error
+    Start Timer  error
+    Sleep   5 seconds
+    Stop Timer    error
+    Run Keyword And Expect Error    STARTS: Difference      Verify Single Timer   3   0    error
 
 LogParser
     parseLogFile
@@ -40,6 +98,14 @@ Foo Test Case
     OpenGoogle
 
 *** Keywords ***
+Benchmark Setup
+  Configure Timer   3 seconds   0 seconds   ${TEST NAME}
+  Start Timer   ${TEST NAME}
+
+Benchmark TearDown
+  Stop Timer    ${TEST NAME}
+  Verify Single Timer    3 seconds   0 seconds   ${TEST NAME}
+
 parseLogFile
      ${list}    Create List
      ${list}   get_unique_task_names       ${CURDIR}${/}tasks_log.txt
@@ -54,7 +120,6 @@ parseLogFile
         Log    ${i}
         Append To File      ${CURDIR}${/}tasks_duration.txt     ${i}${\n}
      END
-
 
 dictVariables
     Log   ${FINNISH.cat}
---
+++ .idea/com/scripts/parse_re_match.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+import re
+import sys
+
+class Task:
+    ''' Object representing an tcs-bank task '''
+
+    def __init__(self, task_name, duration):
+        self.name = task_name
+        self.durations = [int(duration)]
+
+    def average(self):
+        return sum(self.durations)/len(self.durations)
+
+    def __str__(self):
+        return self.name + " durations: " + self.durations + ", avg " + str(self.average()) + " ms"
+
+    def __repr__(self):
+        return self.name + " durations: " + self.durations + ", avg " + str(self.average()) + " ms"
+
+    def min(self):
+        return min(self.durations)
+
+    def __str__(self):
+        return self.name + " durations: " + self.durations + ", min " + str(self.min()) + " ms"
+
+    def __repr__(self):
+        return self.name + " durations: " + self.durations + ", min " + str(self.min()) + " ms"
+
+    def max(self):
+        return max(self.durations)
+
+    def __str__(self):
+        return self.name + " durations: " + self.durations + ", max " + str(self.max()) + " ms"
+
+    def __repr__(self):
+        return self.name + " durations: " + self.durations + ", max " + str(self.max()) + " ms"
+
+
+'''
+tasks = {}
+for line in sys.stdin:
+        match = re.match(r'^.*Execution ended for task ([\S\s]+) for ([\S\s]+) * duration ([\d]+)ms.*', line)
+        if match is not None:
+            task_name = match.groups()[0]
+            duration = match.groups()[2]
+            #names.append(name)
+            if match.groups()[1] is not None:
+                task_name = match.groups()[0]+" "+match.groups()[1]
+        #print(task_name + " " + duration + " ms")
+        task = tasks.get(task_name, None)
+        if task is None:
+            tasks[task_name] = Task(task_name, duration)
+        else:
+            task.durations.append(int(duration))
+for key, task in tasks.items():
+    print(task.name + " avg " + str(task.average()) + " ms"+ " min " + str(task.min()) + " ms"+ " max " + str(task.max()) + " ms")
+'''
+if __name__ == '__main__':
+    log_file = r'C:\Users\timok1\testing\robot-testcases-python3\RobotTesting\com\testing\parse-log\fi-point.log.2020-02-24.txt'
+    #for line in sys.stdin:
+    tasks = {}
+    with open(log_file, "r") as file:
+        for line in file:
+            #match = re.match(r'^.*Execution ended for task ([\S\s]+) for .* duration ([\d]+)ms.*', line)
+            #if match is not None:
+            #    task_name = match.groups()[0]
+            #    duration = match.groups()[1]
+            match = re.match(r'^.*Execution ended for task ([\S\s]+) for ([\S\s]+) * duration ([\d]+)ms.*', line)
+
+            first = ""
+            task_name = ""
+            duration = 0
+            if match is not None:
+                first = match.groups()[0]
+            if not (first.startswith('XML_REPORT') or first.startswith('ACCOUNT_STATEMENT')):
+                if match is not None:
+                    task_name = match.groups()[0]
+                    duration = match.groups()[2]
+                    #names.append(name)
+                    if match.groups()[1] is not None:
+                        task_name = match.groups()[0]+" "+match.groups()[1]
+
+                #print(task_name + " " + duration + " ms")
+
+                task = tasks.get(task_name, None)
+
+                if task is None:
+                    tasks[task_name] = Task(task_name, duration)
+                else:
+                    task.durations.append(int(duration))
+
+    for key, task in tasks.items():
+        print(task.name + " avg " + str(task.average()) + " ms"+ " min " + str(task.min()) + " ms"+ " max " + str(task.max()) + " ms")
